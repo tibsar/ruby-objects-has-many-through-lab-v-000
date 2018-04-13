500439c0cf9f0dcf2f57635b27a45de2ebac0141
Sara Tibbetts <sxt4866@rit.edu>
2018-04-13 00:24:33 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,19 +1,4 @@
 
 class Artist
-  attr_reader :songs
-
-  def initialize
-    @songs = []
-  end
-
-  def add_song(song)
-    @songs << song
-  end
-
-  def genres
-    @songs.collect{ |song| song.genre }.uniq 
-  end 
-
-  #song.genre #=> <Genre > 
 
 end
