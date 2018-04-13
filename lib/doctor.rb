7da7cf7d41dcdbdf37e64bc551787cac10cee280
Sara Tibbetts <sxt4866@rit.edu>
2018-04-13 00:33:30 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,4 +1,14 @@
 class Doctor
   attr_accessor :name
+  attr_reader :appointments
 
+  def initialize
+    @appointments = []
+  end 
+
+  def add_appointment(appointment)
+    @appointments << appointment
+  end 
+
+  
 end
